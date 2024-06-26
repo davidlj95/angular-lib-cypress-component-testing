cc319d06b0c36493704dc286e54f575c934f9496
David LJ <mail@davidlj95.com>
2024-06-27 00:15:48 +0200

    feat: add sample library
    ng g library sample-lib

---
+++ projects/sample-lib/src/lib/sample-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sample-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      sample-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class SampleLibComponent {
+
+}
---
+++ projects/sample-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of sample-lib
+ */
+
+export * from './lib/sample-lib.service';
+export * from './lib/sample-lib.component';
