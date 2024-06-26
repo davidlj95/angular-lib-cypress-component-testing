1103420d57978cafde381349cdfb44318347fe7a
David LJ <mail@davidlj95.com>
2024-06-27 01:38:10 +0200

    test: add manual cypress config

--- projects/sample-lib/cypress.config.ts
+++ projects/sample-lib/cypress.config.ts
@@ -1,18 +1,29 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  
+
   e2e: {
     'baseUrl': 'http://localhost:4200'
   },
-  
-  
+
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
+      options: {
+        projectConfig: {
+          root: '',
+          sourceRoot: 'src',
+          buildOptions: {
+            tsConfig: "tsconfig.lib.json",
+            main: "src/cypress-entrypoint.ts",
+            outputPath: '../../dist/sample-lib'
+          }
+        }
+      }
     },
     specPattern: '**/*.cy.ts'
   }
-  
+
 })
